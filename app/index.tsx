be26478c5ca10462b6320389b73eea7d1407b189
Favio V. F. <favio102@protonmail.com>
2024-08-12 23:41:51 -0700

    profile page added

--- app/index.tsx
+++ app/index.tsx
@@ -2,12 +2,16 @@ import { Text, View } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import "../styles.css";
+import { Link } from "expo-router";
 
 const App = () => {
   return (
     <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-yellow-400 text-2xl">Open up App.js to start working on your app!</Text>
+      <Text className="text-yellow-400 text-2xl">
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
+      <Link href="/profile" className="text-3xl text-blue-600">Profile</Link>
     </View>
   );
 };

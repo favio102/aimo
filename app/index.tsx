1241b633fcf7621e833d75da5af49c2f04051050
Favio V. F. <favio102@protonmail.com>
2024-08-13 06:00:08 -0700

    home page added

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,47 @@
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import "../styles.css";
 import { Link } from "expo-router";
 import { NativeWindStyleSheet } from "nativewind";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "@/constants";
 
 NativeWindStyleSheet.setOutput({ web: "native", default: "native" });
 
 const App = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Text className="font-pextrabold text-2xl">AIMO</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" className="text-3xl text-blue-600">
-        Home
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200">AIMO</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 right-1"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity meets innovations: <br /> embark on a journey of
+            limitless exploration with AIMO.
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 

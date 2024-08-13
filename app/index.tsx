8b231dd77f83ff8476a1bb13d921491e72c7195f
Favio V. F. <favio102@protonmail.com>
2024-08-13 02:31:04 -0700

    constant and assets added

--- app/index.tsx
+++ app/index.tsx
@@ -7,8 +7,8 @@ import { Link } from "expo-router";
 const App = () => {
   return (
     <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-yellow-400 text-2xl">
-        Open up App.js to start working on your app!
+      <Text className="font-pextrabold text-2xl">
+        AIMO
       </Text>
       <StatusBar style="auto" />
       <Link href="/profile" className="text-3xl text-blue-600">Profile</Link>
--- globals.d.ts
+++ globals.d.ts
@@ -1,1 +1,7 @@
 declare module "*.css";
+
+declare module '*.png' {
+    const value: any;
+    export default value;
+  }
+  

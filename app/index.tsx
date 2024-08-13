cf9b2d50e51260f298b02cb1de1c8db96bcd51ac
Favio V. F. <favio102@protonmail.com>
2024-08-13 06:24:39 -0700

    customBtn added

--- app/index.tsx
+++ app/index.tsx
@@ -2,10 +2,12 @@ import { Image, ScrollView, Text, View } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import "../styles.css";
-import { Link } from "expo-router";
+import { Redirect, router } from "expo-router";
 import { NativeWindStyleSheet } from "nativewind";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "@/constants";
+import CustomButton from "@/components/CustomButton";
+import { isLoading } from "expo-font";
 
 NativeWindStyleSheet.setOutput({ web: "native", default: "native" });
 
@@ -13,7 +15,7 @@ const App = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -39,8 +41,14 @@ const App = () => {
             Where Creativity meets innovations: <br /> embark on a journey of
             limitless exploration with AIMO.
           </Text>
+          <CustomButton
+            title="Continue With Email"
+            containerStyles="w-full mt-7"
+            handlePress={() => router.push("/sign-in")}
+          />
         </View>
       </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 };

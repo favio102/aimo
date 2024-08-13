12b556f1677c22aeb51d36a858ce7d20dc0f31bf
Favio V. F. <favio102@protonmail.com>
2024-08-13 02:48:00 -0700

    auth, tabs and seach added

--- app/index.tsx
+++ app/index.tsx
@@ -7,11 +7,11 @@ import { Link } from "expo-router";
 const App = () => {
   return (
     <View className="flex-1 items-center justify-center bg-black">
-      <Text className="font-pextrabold text-2xl">
-        AIMO
-      </Text>
+      <Text className="font-pextrabold text-2xl">AIMO</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" className="text-3xl text-blue-600">Profile</Link>
+      <Link href="/home" className="text-3xl text-blue-600">
+        Home
+      </Link>
     </View>
   );
 };

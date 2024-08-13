21bc93eb8ecc252b7f7b5bd5e87b6e4c811bbaf6
Favio V. F. <favio102@protonmail.com>
2024-08-13 10:02:37 -0700

    formfield added, sign-in page complete

--- app/index.tsx
+++ app/index.tsx
@@ -15,7 +15,7 @@ const App = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center min-h-[85vh] px-4">
+        <View className="w-full justify-center items-center h-full px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
